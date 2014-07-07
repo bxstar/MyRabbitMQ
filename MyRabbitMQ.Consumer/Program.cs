f3639bc7db870006e7ff71147bc4f6807f8b2008
bxstar <yurongsheng@163.com>
2014-07-07 22:26:37 +0800

    建立RabbitMQ项目

---
+++ MyRabbitMQ.Consumer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace MyRabbitMQ.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "115.182.89.51", Port = 5672, UserName = "yrs", Password = "carlos", VirtualHost = "yrs" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("hello", false, false, false, null);
+
+                    var consumer = new QueueingBasicConsumer(channel);
+                    channel.BasicConsume("hello", true, consumer);
+
+                    Console.WriteLine("[*] Waiting for message." + "To exit press CTRL+C");
+
+                    while (true)
+                    {
+                        var ea = (BasicDeliverEventArgs)consumer.Queue.Dequeue();
+
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine("[x] Received {0}", message);
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ MyRabbitMQ.Sender/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace MyRabbitMQ.Sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "115.182.89.51", Port = 5672, UserName = "yrs", Password = "carlos", VirtualHost = "yrs" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("hello", false, false, false, null);
+
+                    string message = string.Format("Hello World {0}", DateTime.Now.ToShortTimeString());
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish("", "hello", null, body);
+                    Console.WriteLine("[x] sent {0}", message);
+                    Console.Read();
+                };
+            };
+
+        }
+    }
+}
